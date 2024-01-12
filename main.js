7b9fec739164889efdafb0d3fffb12a3f6ff3966
Mohamed Fathi <mohamedfathielqastawy@gmail.com>
2024-01-12 22:02:36 +0200

    sign up page

---
+++ main.js
@@ -0,0 +1,50 @@
+let signupName = document.getElementById("signupName");
+let signupEmail = document.getElementById("signupEmail");
+let signupPassword = document.getElementById("signupPassword");
+let signinEmail = document.getElementById("signinEmail");
+let signinPassword = document.getElementById("signinPassword");
+
+let userData = [];
+
+if (localStorage.getItem("userData") == null) {
+  urlList = [];
+} else {
+  userData = JSON.parse(localStorage.getItem("userData"));
+}
+
+function userSignup() {
+  let alertMessage = document.querySelector(".alert-msg");
+  if (validate()) {
+    let user = {
+      userName: signupName.value,
+      userEmail: signupEmail.value,
+      userPassword: signupPassword.value,
+    };
+    userData.push(user);
+    localStorage.setItem("userData", JSON.stringify(userData));
+    clearForm();
+    alertMessage.setAttribute("class", "text-danger m-5 d-none alert-msg");
+  } else {
+    alertMessage.setAttribute("class", "text-danger m-5 d-inline alert-msg");
+  }
+}
+
+let button = document.querySelector(".btn.btn-outline-info");
+
+button.addEventListener("click", userSignup);
+button.addEventListener("click", redirect);
+
+function clearForm() {
+  signupName.value = "";
+  signupEmail.value = "";
+  signupPassword.value = "";
+}
+
+function redirect() {
+  location.href = "/signin.html";
+}
+
+function validate() {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(signupEmail.value);
+}
