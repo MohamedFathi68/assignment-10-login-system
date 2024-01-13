4db2ea05354156dc156364a38ed0058bb52fd733
Mohamed Fathi <mohamedfathielqastawy@gmail.com>
2024-01-13 07:40:08 +0200

    fix bug

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
+// user sign up section
 let signupName = document.getElementById("signupName");
 let signupEmail = document.getElementById("signupEmail");
 let signupPassword = document.getElementById("signupPassword");
-let signinEmail = document.getElementById("signinEmail");
-let signinPassword = document.getElementById("signinPassword");
 
 let userData = [];
-
-if (localStorage.getItem("userData") == null) {
-  urlList = [];
-} else {
-  userData = JSON.parse(localStorage.getItem("userData"));
-}
 
 function userSignup() {
   let alertMessage = document.querySelector(".alert-msg");
@@ -23,15 +16,13 @@ function userSignup() {
     userData.push(user);
     localStorage.setItem("userData", JSON.stringify(userData));
     clearForm();
+    redirectTologin();
   } else {
     alertMessage.classList.replace("d-none", "d-block");
   }
 }
 
-let button = document.querySelector(".btn.btn-outline-info");
-
-button.addEventListener("click", userSignup);
-button.addEventListener("click", redirect);
+let signupButton = document.querySelector(".signup-btn");
 
 function clearForm() {
   signupName.value = "";
@@ -39,11 +30,54 @@ function clearForm() {
   signupPassword.value = "";
 }
 
-function redirect() {
-  location.href = "/signin.html";
+function redirectTologin() {
+  location.href = "/index.html";
 }
 
 function validate() {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   return emailRegex.test(signupEmail.value);
 }
+
+// user log in section
+
+let signinEmail = document.getElementById("signinEmail");
+let signinPassword = document.getElementById("signinPassword");
+
+if (localStorage.getItem("userData") == null) {
+  userData = [];
+} else {
+  userData = JSON.parse(localStorage.getItem("userData"));
+}
+
+let loginEmail = signinEmail.value;
+let loginPassword = signinPassword.value;
+
+let sessionName = [];
+
+function login() {
+  for (let i = 0; i < userData.length; i++) {
+    if (
+      userData[i].userEmail.match(loginEmail.toLowerCase()) &&
+      userData[i].userPassword.match(loginPassword.toLowerCase())
+    ) {
+      localStorage.setItem("sessionName", JSON.stringify(userData[i].userName));
+      
+      redirectToHome();
+    } else {
+      document.getElementById("incorrect").innerHTML =
+        '<span class="p-2 text-danger">incorrect email or password</span>';
+    }
+  }
+  function redirectToHome() {
+    location.href = "/home.html";
+  }
+}
+let n = JSON.parse(localStorage.getItem("sessionName"))
+console.log(n);
+// document.querySelector(".session-name").innerHTML = ;
+
+let loginButton = document.querySelector(".login-btn");
+loginButton.addEventListener("click", login);
+
+console.log(localStorage.getItem("sessionName"));
